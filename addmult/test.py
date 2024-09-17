1d04bfd7b7cb2ade3e3373a96cca07e0f5a997ef
dpinney <david@pinney.org>
2024-09-17 13:33:08 -0400

    rework test infra

--- addmult/test.py
+++ addmult/test.py
@@ -1,4 +1,5 @@
-import __init__
+import os
 
-assert __init__.add(1,1) is 2, '1 plus 1 not equal to 2. Ouch.'
-assert __init__.mult(1,1) is 1, '1 times one not equal 1. Ouch.'
+x = os.listdir('.')
+
+print(x)
--- deployToPypi.sh
+++ deployToPypi.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Deploy this library to pypi.
 # You'll need credentials first from https://pypi.python.org/pypi
+# Needs update to https://packaging.python.org/tutorials/packaging-projects/
 
 python "setup.py" sdist
 python "setup.py" register
